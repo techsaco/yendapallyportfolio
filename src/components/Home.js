8e89f9c76f2151571f8d6133cbb37f0ed438aebd
Techsaco <43525495+techsaco@users.noreply.github.com>
2021-09-21 21:50:22 -0400

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,17 @@
 import React from "react"
 import image from "../marblehomescreen.jpg"
+import pink from "../pinkmarble.jpg"
 
 export default function Home(){
     
     return (
         <main>
-            <img src= {image} alt= "Designedbydivy" className="absolute object-cover w-full h-full"/>
-            <section className = "relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-                
+            <img src= {image} alt= "Designedbydivy" className="relative flex object-cover w-full h-full"/>
+            <img src= {pink} className = "responsive"/>
+            <section className = "relative flex justify-center ">
+            
             </section>
+            
         </main>
     )
 }
